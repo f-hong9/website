0409817a085db16e8f5f02e5c71dd39d98acccac
hong-fred <77351665+hong-fred@users.noreply.github.com>
2022-11-16 03:11:36 -0500

    first commit

---
+++ js/ityped.js
@@ -0,0 +1,6 @@
+ityped.init(document.querySelector("#ityped"), {
+    strings: ['WELCOME TO MY WEBSITE!', 'SCROLL DOWN TO READ ALL ABOUT ME! :)'],
+    showCursor: true,
+    typeSpeed: 80,
+    backSpeed: 60
+})
